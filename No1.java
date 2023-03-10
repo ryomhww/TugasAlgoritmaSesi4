a0d2d1766cff3c086be55f66ed245c8dd27dd275
ryomhww <114847378+ryomhww@users.noreply.github.com>
2023-03-10 15:10:29 +0700

    Add files via upload

---
+++ No1.java
@@ -0,0 +1,7 @@
+public class No1 {
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= 11; i = i + 2) {
+            System.out.print(i);
+        }
+    }
+}
---
+++ No2.java
@@ -0,0 +1,8 @@
+public class No2 {
+    public static void main(String[] args) throws Exception {
+        for (int i = 20; i >= 0; i -= 5) {
+            System.out.print(i);
+        }
+
+    }
+}
